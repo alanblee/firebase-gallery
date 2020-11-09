0ce4a117bff668fcda971897b0a5f72264c0e1cc
alanblee <alanbenlee12@gmail.com>
2020-11-09 12:07:35 -0800

    config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize storage service
+const projectStorage = firebase.storage();
+
+// Initialize db service
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
